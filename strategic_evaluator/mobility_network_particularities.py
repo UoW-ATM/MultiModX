a946e5840779669d12d4ee9efa2dff2b622c8567
Luis <l.delgado@westminster.ac.uk>
2024-05-08 12:35:37 +0100

    Heuristic of air layer work when end nodes  not in layer

--- strategic_evaluator/mobility_network_particularities.py
+++ strategic_evaluator/mobility_network_particularities.py
@@ -20,14 +20,25 @@ def mct_air_network(obj, service_from, service_to):
 
 def fastest_air_time_heuristic(obj, node, destination_nodes):
     from libs.uow_tool_belt.general_tools import haversine
+    distances = []
     for d in destination_nodes:
-        if (node, d) not in obj.distance_origin_destination:
+
+        dist = obj.distance_origin_destination.get((node, d))
+
+        if dist is not None:
+            distances += [dist]
+        elif d in obj.airport_coordinates.icao_id:
             cn = obj.airport_coordinates[obj.airport_coordinates.icao_id == node].iloc[0]
             cd = obj.airport_coordinates[obj.airport_coordinates.icao_id == d].iloc[0]
             dist = haversine(cn.lon, cn.lat, cd.lon, cd.lat)
             obj.distance_origin_destination[(node, d)] = dist
+            distances += [dist]
 
-    min_dist = min(obj.distance_origin_destination.get((node, destination)) for destination in destination_nodes)
+    min_dist = 0
+    if len(distances) > 0:
+        min_dist = min(distances)
+
+    # min_dist = min(obj.distance_origin_destination.get((node, destination)) for destination in destination_nodes)
     min_time = min_dist * timedelta(seconds=8)  # This is actually super fast
     return min_time
 
