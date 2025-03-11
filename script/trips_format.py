4f72f2e0fa71058728afc805945c0f8668a050d7
lmenendez-nommon <lucia.menendez-pidal@nommon.es>
2025-03-11 17:15:54 +0100

    some functions added to trip_format.py, notebook to create international demand created (demand_MD_international.ipynb) and international_flight_analysis.ipynb modified

--- script/trips_format.py
+++ script/trips_format.py
@@ -38,6 +38,7 @@ def remove_useless_columns(trips: pd.DataFrame):
     print(f"{num_removed_columns} columns were removed")
     return trips_final
 
+
 def format_trips(trips: pd.DataFrame, airports_to_NUTS: dict):
     '''
     This is the main function to format trips. It takes as inputs the initial dataframe and a dictionary with
@@ -161,6 +162,7 @@ def remove_abroad_train_trips(trips: pd.DataFrame):
     trips_final=pd.concat([trips_no_abroad,trips_abroad],axis=0)
     return trips_final
 
+
 def remove_numbers_fr_pt(column: pd.Series):
     """This function replaces the zones of france and portugal by FR or PT and 
     the abroad_mcc code by only the code
@@ -175,6 +177,7 @@ def remove_numbers_fr_pt(column: pd.Series):
     # Remove 'abroad_' prefix
     column = column.astype(str).str.replace(r"^abroad_", "", regex=True)
     return column
+
 
 def map_mcc_to_country(zone, mcc_to_country: dict):
     """This function changes the origin or destination zone of a trips 
@@ -195,6 +198,7 @@ def map_mcc_to_country(zone, mcc_to_country: dict):
         return mcc_to_country.get(int(zone),zone)
     else:
         return zone
+    
 
 def precise_origin_destination(trips_abroad: pd.DataFrame):
     """This function changes the name of the origin and the destination
@@ -219,6 +223,7 @@ def precise_origin_destination(trips_abroad: pd.DataFrame):
     trips_abroad = trips_abroad.drop(columns=["origin_zone", "destination_zone"])
 
     return trips_abroad
+
 
 def format_trips_abroad(trips_abroad: pd.DataFrame, mcc_to_country: dict) -> pd.DataFrame:
     """This is the main function for formatting the trips from or to abroad.
@@ -265,6 +270,7 @@ def format_trips_abroad(trips_abroad: pd.DataFrame, mcc_to_country: dict) -> pd.
 
     return trips_abroad_final
 
+
 def format_trips_national(trips_national: pd.DataFrame): 
     """This function formats the national trips to match them to the 
     international ones
@@ -286,6 +292,7 @@ def format_trips_national(trips_national: pd.DataFrame):
     trips_national=trips_national.groupby(cols,as_index=False, dropna=False)['trips'].sum()    
 
     return trips_national
+
 
 def rescale_trips(trips_abroad:pd.DataFrame,coeffs_incoming: pd.DataFrame,coeffs_outgoing: pd.DataFrame):
     """This function rescales the international trips with the coefficients
@@ -326,3 +333,233 @@ def rescale_trips(trips_abroad:pd.DataFrame,coeffs_incoming: pd.DataFrame,coeffs
                 trips_abroad_final.at[idx,col]=row[col]*coeffs_outgoing_right_index["real_vs_predicted_coeff"].loc[destination]
 
     return trips_abroad_final
+
+
+def format_airports(airport:str,iata_to_icao:dict):
+    """This function changes the entry of exit_point or entry_point of a dataframe
+    from airport_XXX where XX is the IATA code, to the ICAO code
+        
+    Inputs:
+        airport string 
+        iata_to_icao dictionary
+    
+    Outputs:
+        modified string
+    """
+    if pd.notna(airport):
+        components=airport.split("_")
+        airport=iata_to_icao[components[1]]
+    return airport
+
+
+def map_and_aggregate_flight_capacity(flight_schedules: pd.DataFrame,
+                                      airport_to_country: dict,
+                                      selected_countries=None
+                                      ):
+    """Aggregates the flight capacity per origin and destination airports and filters
+    by selected countries (if provided)
+    
+    Args:
+        flight_schedules: dataframe that contains information about the flight schedules on a given day
+        airport_to_country: dictionary with airports and country acronym
+        selected_countries: set or list of selected countries
+
+    Returns: 
+        flight_capacity: dataframe with the information
+    """
+    # sums the seats per airports in flight_schedules
+    flights_capacity=flight_schedules.groupby(["origin","destination"])["seats"].sum().reset_index()
+
+    # adds country of origin and destination
+    flights_capacity["country_origin"]=flights_capacity["origin"].map(airport_to_country)
+    flights_capacity["country_destination"]=flights_capacity["destination"].map(airport_to_country)
+
+    # selects only the selected countries
+    if selected_countries is not None:
+        flights_capacity=flights_capacity[
+            (flights_capacity["country_origin"].isin(selected_countries))|
+            (flights_capacity["country_destination"].isin(selected_countries))]
+        
+    return flights_capacity
+
+
+def aggregate_seats_by_airport_and_country(flights_capacity):
+    """Aggregates seat counts by airport and country, categorising flights based on whether the destination is Spain ("ES").
+    
+    Args: 
+        flights_capacity: dataframe with information about the airports of origin and destination 
+        and their country
+
+    Returns:
+        a tuple containing two dictionaries, seats_from Aggregated seats for flights with destination country "ES"
+        and seat_to Aggregated seats for flights with origin country not "ES"
+    """
+    # initialise
+    seats_from={}
+    seats_to={}
+
+    for _,row in flights_capacity.iterrows():
+        # check whether it is an incoming or an outgoing trip
+        if row["country_destination"]=="ES":
+
+            # select the airport and country
+            airport=row["destination"]
+            country=row["country_origin"]
+
+            # assign seats per country
+            if airport not in seats_from.keys():
+                seats_from[airport]={country:row["seats"]}
+            else:
+                dict=seats_from[airport]
+                if country not in dict.keys():
+                    dict[country]=row["seats"]
+                else:
+                    dict[country]+=row["seats"]
+        else:
+            
+            # select the airport and the country
+            airport=row["origin"]
+            country=row["country_destination"]
+
+            # assign seats per country
+            if airport not in seats_to.keys():
+                seats_to[airport]={country:row["seats"]}
+            else:
+                dict=seats_to[airport]
+                if country not in dict.keys():
+                    dict[country]=row["seats"]
+                else:
+                    dict[country]+=row["seats"]
+
+    return seats_from, seats_to
+
+
+def assign_airport_incoming(trips_incoming: pd.DataFrame, 
+                            flights_capacity: pd.DataFrame, 
+                            seats_from: dict):
+    """Recallibrates the number of trips according to the information of the airports
+    
+    Args:
+        trips_incoming dataframe: dataframe with information about the trips coming from abroad to spain
+        flight_capacity: dataframe with information about the seat capacity from spanish airport to airport in origin
+        seats_from: obtained from flight_capacity using the aggregate_seats_by_airport_and_country function
+
+    Returns: 
+        trips_final: the modified dataframe
+    """
+    
+    #merge and format dataframe
+    trips_final=pd.merge(left=trips_incoming, left_on=["origin","entry_point"],right=flights_capacity,right_on=["country_origin","destination"],how="left")
+    trips_final=trips_final.rename({"origin_x":"origin","destination_x":"destination"},axis=1)
+    trips_final["origin"]=trips_final["origin_y"].fillna(trips_final["origin"])
+    trips_final=trips_final.drop(["origin_y"],axis=1)
+    trips_final["proportionality_coeff"]=float(1)
+
+    # find the proportionality_coeff
+    for idx,row in trips_final.iterrows():
+        if row["entry_point"] in seats_from:
+            if row["origin_name"] in seats_from[row["entry_point"]]:
+                coeff=row["seats"]/seats_from[row["entry_point"]][row["origin_name"]]
+
+                trips_final.at[idx, "proportionality_coeff"]=coeff
+
+    # apply the proportionality_coeff to the relevant columns
+    cols=[f"archetype_{n}" for n in range(6)]
+    cols.append("trips")
+    for col in cols:
+        trips_final[col]=trips_final[col]*trips_final["proportionality_coeff"]
+
+    #remove columns that we don't need
+    trips_final=trips_final.drop(["country_origin","country_destination","destination_y"],axis=1)
+
+    #drop trips that were not assigned (note that then the sum of the trips will not match)
+    trips_final=trips_final[trips_final["seats"].notna()]
+
+    return trips_final
+
+def assign_airport_outgoing(trips_outgoing: pd.DataFrame, 
+                            flights_capacity: pd.DataFrame, 
+                            seats_to: dict):
+    """Recallibrates the number of trips according to the information of the airports
+    
+    Args:
+        trips_outgoing dataframe: dataframe with information about the trips going abroad from spain
+        flight_capacity: dataframe with information about the seat capacity from spanish airport to airport in destination
+        seats_to: obtained from flight_capacity using the aggregate_seats_by_airport_and_country function
+
+    Returns: 
+        trips_final: the modified dataframe
+    """
+    
+    #merge and format dataframe
+    trips_final=pd.merge(left=trips_outgoing, left_on=["destination","exit_point"],right=flights_capacity,right_on=["country_destination","origin"],how="left")
+    trips_final=trips_final.rename({"origin_x":"origin","destination_x":"destination"},axis=1)
+    trips_final["destination"]=trips_final["destination_y"].fillna(trips_final["origin"])
+    trips_final=trips_final.drop(["destination_y"],axis=1)
+    trips_final["proportionality_coeff"]=float(1)
+
+    # find the proportionality_coeff
+    for idx,row in trips_final.iterrows():
+        if row["exit_point"] in seats_to:
+            if row["destination_name"] in seats_to[row["exit_point"]]:
+                coeff=row["seats"]/seats_to[row["exit_point"]][row["destination_name"]]
+
+                trips_final.at[idx, "proportionality_coeff"]=coeff
+
+    # apply the proportionality_coeff to the relevant columns
+    cols=[f"archetype_{n}" for n in range(6)]
+    cols.append("trips")
+    for col in cols:
+        trips_final[col]=trips_final[col]*trips_final["proportionality_coeff"]
+
+    #remove columns that we don't need
+    trips_final=trips_final.drop(["country_origin","country_destination","origin_y"],axis=1)
+
+    #drop trips that were not assigned (note that then the sum of the trips will not match)
+    trips_final=trips_final[trips_final["seats"].notna()]
+
+    return trips_final  
+
+def trips_format_to_pipeline(trips):
+    """
+    Processes the trips DataFrame to create a combined DataFrame with archetype-specific trip counts.
+
+    Args:
+        trips (pd.DataFrame): Input DataFrame containing trip data with archetype columns.
+
+    Returns:
+        pd.DataFrame: Combined DataFrame with columns: date, origin, destination, archetype, trips.
+    """
+    # Create a dictionary to store the DataFrames
+    dataframes = {}
+
+    # Generate the DataFrames dynamically
+    for i in range(6):
+        df_name = f"df_{i}"
+        archetype_col = f"archetype_{i}"
+        dataframes[df_name] = trips.groupby(["date", "origin", "destination"], as_index=False).agg({archetype_col: "sum"})
+
+    # Create a list of tuples containing the DataFrames and their corresponding archetype columns
+    dataframe_list = [
+        (dataframes[f"df_{i}"], f"archetype_{i}")
+        for i in range(6)
+    ]
+
+    # Combine the DataFrames with a new "archetype" column
+    combined_df = pd.concat(
+        [
+            df.assign(archetype=archetype_col)  # Add new archetype column
+            .rename(columns={archetype_col: 'trips'})  # Rename archetype column value to "trips"
+            for df, archetype_col in dataframe_list
+        ],
+        ignore_index=True
+    )
+
+    # Reorder columns
+    columns = ["date", "origin", "destination", "archetype", "trips"]
+    combined_df = combined_df[columns]
+
+    # Sort the combined DataFrame
+    combined_df = combined_df.sort_values(by=["origin", "destination", "archetype"])
+
+    return combined_df
