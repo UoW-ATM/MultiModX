44300d4d867205bcc829315cf6ea04da5d64f616
Michal Weiszer <m.weiszer@westminster.ac.uk>
2025-04-10 11:19:11 +0100

    debug

--- performance_indicators/mmx_kpis.py
+++ performance_indicators/mmx_kpis.py
@@ -9,16 +9,16 @@ import matplotlib.pyplot as plt
 from kpi_lib_strategic import strategic_total_journey_time, diversity_of_destinations, modal_share, pax_time_efficiency, demand_served, load_factor, resilience_alternatives, catchment_area, cost_per_user,co2_emissions, buffer_in_itineraries, seamless_of_travel, pax_processes_time
 from kpi_lib_tactical import flight_arrival_delay, kerb2gate_time, total_journey_time, variability
 
-def read_strategic_output(path_to_strategic_output):
-	pax_assigned_to_itineraries_options = pd.read_csv(Path(path_to_strategic_output) / 'pax_assigned_to_itineraries_options_0.csv')
-	possible_itineraries_clustered_pareto_filtered = pd.read_csv(Path(path_to_strategic_output) / 'possible_itineraries_clustered_pareto_filtered_0.csv')
-	demand = pd.read_csv(Path(path_to_strategic_output) / '..' / '..' / 'demand'/'demand_ES_MD_intra_v0.4.csv')
-	pax_assigned_seats_max_target = pd.read_csv(Path(path_to_strategic_output) / 'pax_assigned_seats_max_target_0.csv')
-	pax_assigned_tactical = pd.read_csv(Path(path_to_strategic_output) / 'pax_assigned_tactical_0.csv')
-	pax_assigned_tactical_not_supported = pd.read_csv(Path(path_to_strategic_output) / 'pax_assigned_tactical_not_supported_0.csv')
-	rail_timetable_proc = pd.read_csv(Path(path_to_strategic_output) / '..' / 'rail_timetable_proc_0.csv')
-	flight_schedules_proc = pd.read_csv(Path(path_to_strategic_output) / '..' / 'flight_schedules_proc_0.csv')
-	nuts_regional_archetype_info = pd.read_csv(Path(path_to_strategic_output) / '..' / '..' / 'nuts_regional_archetype_info.csv')
+def read_strategic_output(path_to_strategic_output,preprocessed_version):
+	pax_assigned_to_itineraries_options = pd.read_csv(Path(path_to_strategic_output) / ('pax_assigned_to_itineraries_options_'+preprocessed_version+'.csv'))
+	possible_itineraries_clustered_pareto_filtered = pd.read_csv(Path(path_to_strategic_output) / ('possible_itineraries_clustered_pareto_filtered_'+preprocessed_version+'.csv'))
+	demand = pd.read_csv(Path(path_to_strategic_output) / '..' / '..' / '..' / 'demand'/'demand_ES_MD_intra_v0.4.csv')
+	pax_assigned_seats_max_target = pd.read_csv(Path(path_to_strategic_output) / ('pax_assigned_seats_max_target_'+preprocessed_version+'.csv'))
+	pax_assigned_tactical = pd.read_csv(Path(path_to_strategic_output) / ('pax_assigned_tactical_'+preprocessed_version+'.csv'))
+	pax_assigned_tactical_not_supported = pd.read_csv(Path(path_to_strategic_output) / ('pax_assigned_tactical_not_supported_'+preprocessed_version+'.csv'))
+	rail_timetable_proc = pd.read_csv(Path(path_to_strategic_output) / '..' / ('rail_timetable_proc_'+preprocessed_version+'.csv'))
+	flight_schedules_proc = pd.read_csv(Path(path_to_strategic_output) / '..' / ('flight_schedules_proc_'+preprocessed_version+'.csv'))
+	nuts_regional_archetype_info = pd.read_csv(Path(path_to_strategic_output) / '..' / '..' /'..' / 'nuts_regional_archetype_info_v0.2.csv')
 
 	data = {'pax_assigned_to_itineraries_options':pax_assigned_to_itineraries_options, 'possible_itineraries_clustered_pareto_filtered':possible_itineraries_clustered_pareto_filtered, 'demand':demand, 'pax_assigned_seats_max_target':pax_assigned_seats_max_target,'pax_assigned_tactical':pax_assigned_tactical,'pax_assigned_tactical_not_supported':pax_assigned_tactical_not_supported, 'rail_timetable_proc':rail_timetable_proc,'flight_schedules_proc':flight_schedules_proc, 'nuts_regional_archetype_info':nuts_regional_archetype_info}
 	return data
@@ -92,6 +92,7 @@ if __name__ == '__main__':
 	parser.add_argument('-tf', '--toml_file', help='TOML defining the indicator configuration', required=False, default='mmx_kpis.toml')
 	parser.add_argument('-ex', '--experiment', help='Folder with the experiment', required=False)
 	parser.add_argument('-c','--compare', nargs='+', help='Compare experiments', required=False)
+	parser.add_argument('-ppv', '--preprocessed_version', nargs='+', help='Preprocessed version of schedules to use', required=False, default=['0'])
 
 	# Examples of usage
 	#python3 mmx_kpis.py -ex processed_cs10.pp00.so00_c1
@@ -105,14 +106,15 @@ if __name__ == '__main__':
 	if args.experiment is not None:
 		config['input']['path_to_strategic_output'] = Path(config['input']['path_to_strategic_output']) / args.experiment / 'paths_itineraries'
 		config['output']['path_to_output'] = Path(config['output']['path_to_output']) / args.experiment / 'indicators'
+		config['input']['preprocessed_version'] = args.preprocessed_version[0]
 
 	print ('KPI calculation... ')
-
+	print(args)
 	print(config)
 
 	if args.experiment is not None:
 		recreate_output_folder(Path(config['output']['path_to_output']))
-		data_strategic = read_strategic_output(config['input']['path_to_strategic_output'])
+		data_strategic = read_strategic_output(config['input']['path_to_strategic_output'],config['input']['preprocessed_version'])
 
 
 		data_tactical = read_tactical_data(config['input']['path_to_tactical_output'],config['input']['path_to_tactical_input'])
